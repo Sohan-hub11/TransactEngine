f16fa2d4d1d3c84f483536f25d07ae8dda998cca
Sohan Samanta <sohansamanta18@gmail.com>
2026-04-04 21:11:31 +0530

    v2

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+function connectDB() {
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log('Connected Server to MongoDB Successfully');
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+      process.exit(1); //exiting from server if there is an error in connecting to database.
+    });
+
+}
+
+module.exports = connectDB;
